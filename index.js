436a75cc417a06d3f0cc00cee2f5c8b66beda10c
bexearn12 <earnshr@gmail.com>
2021-06-14 13:21:33 -0500

    commit tree c99fbd2b879589d9297e61fa852f99cbb184f785

--- index.js
+++ index.js
@@ -1,2 +1,236 @@
+// 1. Create a variable called `multiply` set to an equation that will multiply the
+//    variales `num1` and `num2`; the result of the multiplication should be `62`.
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+
+
+// 2. Create a variable called `random` that will generate a random integer greater
+//    than 0.
+//    Math.random() returns a double type pseudo-random number, greater
+//     than or equal to zero and less than one.
+
+const random = Math.floor(Math.random() + 1)
+
+//3.mod  Is an equation that calculates a remainder that is equal to 4
+// 18/7 remainder = 4; Provides the remainder of the next leftover from dividing 2 numbers
+const num3 = 18
+const num4 = 7
+const mod = num3 % num4
+
+//4 max Return 20 as the highest number in the set
+//  Zero or more numbers among which the largest value
+//   will be selected and returned. 20 is greater than 10,
+const max = Math.max(10, 20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. Create a variable called `mod` set to an equation that will calculate the
+//    remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+
+
+
+
+
+
+
+
+// 4. Create a variable called `max` that finds the highest number in a set; the
+//    value returned should be `20`.
